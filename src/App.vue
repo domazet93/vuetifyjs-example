afa107a4205321c1805e10a82699572c40eee2aa
Toni <toni.domaz93@hotmail.com>
2016-12-20 22:39:06 +0100

    Vue and Vuetify install

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">   
+    <home></home>
+  </div>
+</template>
+
+<script>
+import Home from './components/Home'
+
+export default {
+  name: 'app',
+  components: {
+    Home
+  }
+}
+</script>
+
---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <v-app top-navbar footer right-fixed-sidebar>
+      <main>
+        <v-sidebar id="sidebar" fixed right>
+           <img src="../assets/vuetify.png" class="mb-5">
+        </v-sidebar>
+        <v-content>
+          <v-container>
+            <h2>Content</h2>
+          </v-container>
+        </v-content>
+      </main>
+    </v-app>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
+
